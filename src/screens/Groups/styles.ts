823b8dbf7d1ecae7b999f0e8749990dc204ab140
Matheus Amon <amonmatheus757@gmail.com>
2024-05-24 10:36:55 -0300

    feat: add theme from design system

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,13 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View `
     flex: 1;
-    background-color: #2B2B2B;
-    align-items: center;
-    justify-content: center;
-`;
-
-export const Title = styled.Text `
-    color: #FFF;
-    font-size: 32px;
-    font-weight: bold;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+    padding: 24px;
 `;
