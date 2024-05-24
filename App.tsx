26d58b47a080825e14420ba6d26615b4c870d87a
Matheus Amon <amonmatheus757@gmail.com>
2024-05-24 19:29:57 -0300

    style: remove scroll bar from flat list in groups component

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components"
 import { useFonts, Roboto_400Regular,Roboto_700Bold } from "@expo-google-fonts/roboto";
 
-import { Players } from "@screens/Players";
+import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 import { Loading } from "@components/Loading";
 
@@ -21,7 +21,7 @@ export default function App() {
       />
       {
         fontsLoaded 
-          ? <Players />
+          ? <Groups />
           : <Loading />
       }
     </ThemeProvider>
